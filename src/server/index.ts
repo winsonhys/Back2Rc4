9cef218699f885a7f8b07dbe53074ad176a1f97d
Winson <winsonheng@hotmail.com>
2018-06-30 15:38:32 +0800

    added access control

--- src/server/index.ts
+++ src/server/index.ts
@@ -26,6 +26,29 @@ const createServer = async environment => {
   });
 
   //Middleware
+  app.use((req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    // Request methods you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", 1);
+
+    // Pass to next layer of middleware
+    next();
+  });
   app.use(BodyParser.json()); //Allows parsing of JSON http requests
   app.use(ExpressValidator());
 
