57281d8ee8aaf5f282db619e364377690b778631
Winson <winsonheng@hotmail.com>
2018-07-09 00:22:58 +0800

    Error handling

--- src/server/events/handler/editEvents.ts
+++ src/server/events/handler/editEvents.ts
@@ -10,23 +10,31 @@ const editEvents = async (req, res) => {
     return res.status(400).send("Not valid event id");
   }
   const { id } = req.body;
-  const event = await Events.findOne({
-    where: {
-      id
-    }
-  });
-  const { title, start, end, type, allDay, location } = req.body;
-  const updatedEvent = _.omit(req.body, "id");
+  let event;
   try {
-    await event.update({
-      ...event,
-      ...updatedEvent
+    event = await Events.findOne({
+      where: {
+        id
+      }
     });
-    await event.reload();
-    res.send(event);
   } catch (e) {
     console.error(e);
-    res.status(400).send("Unable to update");
+    return res.status(404).send("Cannot find event with this id");
+  }
+  if (event) {
+    const { title, start, end, type, allDay, location } = req.body;
+    const updatedEvent = _.omit(req.body, "id");
+    try {
+      await event.update({
+        ...event,
+        ...updatedEvent
+      });
+      await event.reload();
+      res.send(event);
+    } catch (e) {
+      console.error(e);
+      res.status(400).send("Unable to update");
+    }
   }
 };
 
